81d22c627c6cad1baf1c9c149a0aa6f30e4635fe
yagojf <yagojf23@gmail.com>
2017-11-16 18:56:02 -0200

    Site em homenagem ao GrandChase
    Feito com React, bootstrap

---
+++ gc-player/src/App/index.js
@@ -0,0 +1,80 @@
+import React from 'react';
+
+export default class Index extends React.Component{
+
+    render(){
+        
+                return(<div>
+
+                    <div>
+                    <nav class="navbar navbar-inverse">
+                    <ul class="nav nav-tabs">
+  <li role="presentation" class="active"><a href="#">Inicio</a></li>
+  <li role="presentation"><a href="#">Sobre o Jogo</a></li>
+  <li role="presentation"><a href="#">Personagens</a></li>
+  <li role="presentation"><a href="#">Mapas e cidades</a></li>
+  <li role="presentation"><a href="#">Jogue agora</a></li>
+  <li role="presentation"><a href="#">Contato</a></li>
+  
+  
+</ul>
+  ...
+</nav>
+                    </div>
+ <div class="main">
+      <div class="container">
+        <h1> AVENTURE-SE</h1>
+		<a class="btn"	href="#" type="button"> "VAMOS JOGAR" </a>
+	
+      </div>
+    </div>
+
+    <div class="supporting">
+      <div class="container">
+        <div class="col">
+          <h2>CONHECA</h2>
+          <p>Veja a História da Elesys em busca da redenção.</p>
+        </div>
+        <div class="col">
+          <h2>LUTE</h2>
+          <p>Entre na guerra contra o mal</p>
+        </div>
+        <div class="col">
+          <h2>DOMINE</h2>
+          <p>Venca todos os continentes do game</p>
+        </div>
+        <div class="clearfix"></div>
+      </div>
+    </div>
+
+    <div class="feature">
+      <div class="container">
+        <h2>Monte sua Equipe.</h2>
+      </div>
+    </div>
+    <div class="supporting">
+      <div class="container">
+        <h2>Go Premium</h2>
+        <p>Adquira as moedas.</p>
+		
+		<a class="btn"	href="#" type="button"> "Sobre o jogo"</a>
+		<h2>Sinta a magia acontecer</h2>
+		<center></center>
+        </div>
+
+        </div>
+        <div class="footer">
+      <div class="container">
+        <h2>Stop scrolling. Start moving.</h2>
+
+			<a class="btn"	href="#" type="button"> "Start Now" </a>
+
+      </div>
+    </div>
+  
+                    </div>
+
+
+                )}
+        
+}
